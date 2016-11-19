a1853affa52da366506fd28e575b6d7cabe62002
Jordi Montes <jomsdev@gmail.com>
2016-11-19 13:33:19 -0800

    Int.h file added
    Int extends from Element.h and is the basic int type in the heron tuples.

---
+++ heron/api/src/cpp/types/Int.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __INT_HERON_TYPE_H_
+#define __INT_HERON_TYPE_H_
+
+#include <sstream>
+#include "Element.h"
+
+//TODO Jordi: Include Cereal from the right path
+#include "cereal/archives/binary.hpp"
+#include "cereal/types/polymorphic.hpp"
+
+
+//TODO Jordi: Should I create an Int.cpp ?
+class Int : public Element {
+
+    int _value;
+    
+public:
+    Int() {}
+
+    Int(int value) : _value(value) {}
+
+    int getValue() {
+        return _value;
+    } 
+
+    void save(cereal::BinaryOutputArchive &oarchive) const {
+        oarchive(_value);
+    }
+
+    void load(cereal::BinaryInputArchive &iarchive) {
+        iarchive(_value);
+    }
+
+};
+
+CEREAL_REGISTER_TYPE(Int);
+CEREAL_REGISTER_POLYMORPHIC_RELATION(Element, Int);
+
+
+#endif
